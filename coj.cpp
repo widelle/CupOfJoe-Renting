692f44fe36558876b53bbcff521f589687579a44
widelle <widellebacud@gmail.com>
2025-05-12 11:28:50 +0800

    Add files via upload

---
+++ coj.cpp
@@ -0,0 +1,344 @@
+// renting coj songs
+#include <stdio.h>
+
+void cupofjoe(int song){
+    if(song == 1){ //Nag-Iisang Muli
+        const char *cupofjoe[] = {
+            "Patuloy na aasa na ikaw ay makilala na",
+            "Ng puso kong naghihintay",
+            "Na makasama ka sa'king buhay",
+            "Hanggang sa dulo ng walang hanggan",
+            "Ikaw ang hanggan",
+            "Patuloy na inaasam na masilayan na kita",
+            "Babangon at lalaban at isisigaw ko kay Bathala",
+            "Ang kahilingang mahanap kita",
+            "Ang tanging hangad ng puso'y ikaw",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 2){ //Sinderela
+        const char *cupofjoe[] = {
+            "Wala nang hininga",
+            "Ako'y nanghihina",
+            "Pero 'di alintana",
+            "Oh kahit ako'y matapilok",
+            "Tuloy pa rin ang pagtibok",
+            "Ng puso kong",
+            "Sinisigaw pangalan mo",
+            "Huminto na at 'wag lalayo, oh, oh, oh",
+            "Malapit nang mapatingin",
+            "Ika'y mapapasa'kin din",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 3){ //Alas Dose
+        const char *cupofjoe[] = {
+            "Alas dose na, sinta",
+            "Oras na para tayo ay magsama",
+            "Alas dose na, sinta",
+            "Kulang na kulang ang Pasko kung wala ka",
+            "Pwede bang sabay nating kumpletuhin ang simbang gabi?",
+            "Upang sa mga susunod na alas dose ay ikaw",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 4){ //Sagada
+        const char *cupofjoe[] = {
+            "Sagada, kung sa'n lahat ay isinigaw",
+            "Sa langit na ikaw ay muling matanaw",
+            "Sagada, ang tagpuan kung sa'n itinakdang magwakas",
+            "Ang kwento ng ako at ikaw",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 5){ //Hayaan
+        const char *cupofjoe[] = {
+            "Ipikit ang mga mata, hayaang maglakbay",
+            "Isipang gulo at 'di pa sanay na ika'y",
+            "Wala na",
+            "Na ika'y wala na",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 6){ //Bukod-Tangi
+        const char *cupofjoe[] = {
+            "Nag-iisa ka",
+            "Wala nang iba",
+            "Lunurin mo ako sa iyong piling",
+            "Oh, pwede bang dinggin aking hiling?",
+            "Nag-iisa ka",
+            "Wala nang iba",
+            "Sa iyong tahimik na tingin",
+            "Nadarama lahat ng damdamin",
+            "Nag-iisa ka",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 7){ //Ikaw Pa Rin Ang Pipiliin Ko
+        const char *cupofjoe[] = {
+            "Ngunit kahit saan ka mapadpad",
+            "Kahit ilang taon ang lumipas",
+            "Kahit ika'y mapalayo sa piling ko",
+            "Ikaw pa rin ang pipiliin ko",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 8){ //Mananatili
+        const char *cupofjoe[] = {
+            "Mananatili",
+            "Sa iyong tabi magdamag",
+            "Ating paligid",
+            "Hindi na natin napapansin, gabi ay atin",
+            "Kasabay ng puso at tugtugan",
+            "Pag-indak ng ating katawan",
+            "Mananatili"
+            "Hindi tiyak ang bukas natin",
+            "Ngunit itong gabi ay atin",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 9){ //Estranghero
+        const char *cupofjoe[] = {
+            "Ako ay litong-lito, saan ba ito patungo?",
+            "'Di ko na alam kung dapat bang ihinto",
+            "'Di alam ang sasabihin",
+            "'Di alam ang aking gagawin",
+            "Kung mananatili sa'yo",
+            "Minamahal kong estranghero",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 10){ //Tataya
+        const char *cupofjoe[] = {
+            "'Wag ka nang magduda pa",
+            "'Di mapagkakaila",
+            "Magkaiba man ang ating mundo",
+            "Tataya pa rin sa'yo",
+            "Saan man mapadpad",
+            "Madala o malipat",
+            "Magkalayo man ang ating mundo",
+            "Tataya pa rin sa'yo",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 11){ //Tingin
+        const char *cupofjoe[] = {
+            "'Di pinapansin, ingay sa tabi",
+            "Magulong kapaligiran",
+            "Sa'yo lang ang tingin",
+            "'Di pinapansin, ika'y paiikutin",
+            "Nang dahan-dahan lang",
+            "Sa gitna man ng daan",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 12){ //Wag na Lang
+        const char *cupofjoe[] = {
+            "'Pag sinabi mong 'wag muna",
+            "Ibig sabihin mo ba 'wag na lang?",
+            "Sa tuwing lalapit",
+            "Lumalaki lang ang pagitan ng ating mga puso",
+            "At ang isipan ay litong-lito na",
+            "Kung 'di buo'ng loob mo",
+            "Maaari namang 'wag na lang",
+            "'Wag na lang",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 13){ //Patutunguhan
+        const char *cupofjoe[] = {
+            "Diretso lang tingin sa hilaga",
+            "Abot-tanaw ang iyong hiwaga",
+            "Timog, silangan at kanluran",
+            "Ikaw pa rin ang patutunguhan",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 14){ //Lahat ng Bukas
+        const char *cupofjoe[] = {
+            "Lahat ng bukas ay ibibigay",
+            "Hawaka't 'wag bitawan aking kamay",
+            "Takot ay ibubura",
+            "Pangako magtiwala na",
+            "Lahat ng bukas ay ibibigay",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 15){ //Misteryoso
+        const char *cupofjoe[] = {
+            "Hindi alam kung bakit ba ‘ko nabighani sa iyo",
+            "Hindi alam kung bakit ba gan’to kahit na alam kong",
+            "Mukhang wala rin naman itong patutunguhan",
+            "Kahit pa ilaan lahat ng oras sa 'yo",
+            "Hindi titigil hanggang ako na ang dahilan ng 'yong ngiting",
+            "Misteryoso",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }else if(song == 16){ //Multo
+        const char *cupofjoe[] = {
+            "Hindi na makalaya, dinadalaw mo 'ko bawat gabi",
+            "Wala mang nakikita, haplos mo'y ramdam pa rin sa dilim",
+            "Hindi na na-nanaginip, hindi na ma-makagising",
+            "Pasindi na ng ilaw",
+            "Minumulto na 'ko ng damdamin ko, ng damdamin ko",
+            NULL
+        };
+        for(int i = 0; cupofjoe[i] != NULL; i++) {
+            puts(cupofjoe[i]);
+        }
+    }
+}
+    
+int main(){
+
+    int song = 1; 
+    float amount;
+    float prices [] = {50, 40, 30, 42, 51, 35, 60, 63, 25, 32, 45, 47, 61, 52, 22, 36};
+
+    printf("-CUP OF JOE Discography-\n");
+    printf("1. Nag-Iisang Muli\n");
+    printf("2. Sinderela\n");
+    printf("3. Alas Dose\n");
+    printf("4. Sagada\n");
+    printf("5. Hayaan\n");
+    printf("6. Bukod-Tangi\n");
+    printf("7. Ikaw Pa Rin Ang Pipiliin Ko\n");
+    printf("8. Mananatili\n");
+    printf("9. Estranghero\n");
+    printf("10. Tataya\n");
+    printf("11. Tingin\n");
+    printf("12. Wag na Lang\n");
+    printf("13. Patutunguhan\n");
+    printf("14. Lahat ng Bukas\n");
+    printf("15. Misteryoso\n");
+    printf("16. Multo\n");
+
+    printf("Select a song to rent (1-16 to continue, 17 to exit): "); 
+		if (scanf("%d", &song) != 1) {
+	        printf("Invalid!");
+	        return 0; 
+	    }
+    
+    if(song == 17){
+        printf("Exit!");
+        return 0; 
+    }
+
+    if(song < 1 || song > 16){
+        printf("Invalid number!");
+        return 0;
+    }
+
+     switch(song){
+         case 1:
+             printf("You've selected Nag-Iisang Muli - $50\n");
+             break;
+         case 2:
+             printf("You've selected Sinderela - $40\n");
+             break;
+         case 3:
+             printf("You've selected Alas Dose - $30\n");
+             break;
+         case 4:
+             printf("You've selected Sagada - $42\n");
+             break;
+         case 5:
+             printf("You've selected Hayaan - $51\n");
+             break;
+         case 6:
+             printf("You've selected Bukod-Tangi - $35\n");
+             break;
+         case 7:
+             printf("You've selected Ikaw Pa Rin Ang Pipiliin Ko - $60\n");
+             break;
+         case 8:
+             printf("You've selected Mananatili - $63\n");
+             break;
+         case 9:
+             printf("You've selected Estranghero - $25\n");
+             break;
+         case 10:
+             printf("You've selected Tataya - $32\n");
+             break;
+         case 11:
+             printf("You've selected Tingin - $45\n");
+             break;
+         case 12:
+             printf("You've selected Wag Na Lang - $47\n");
+             break;
+         case 13:
+             printf("You've selected Patutunguhan - $61\n");
+             break;
+         case 14:
+             printf("You've selected Lahat ng Bukas - $52\n");
+             break;
+         case 15:
+             printf("You've selected Misteryoso - $22\n");
+             break;
+         case 16:
+             printf("You've selected Multo - $36\n");
+             break;
+     }
+
+    printf("ENTER YOUR MONEY: $"); 
+    	if(scanf("%f", &amount) != 1){
+    		printf("Invalid!");
+    		return 0;
+		}
+
+    float change = amount - prices[song - 1]; 
+    float addMoney = prices[song - 1] - amount;
+
+	if (amount >= prices[song - 1]){
+        printf("YOUR CHANGE: $%.2f\n", change);    
+    }else{
+        printf("NOT ENOUGH MONEY. ADD MORE: $%.2f\n", addMoney);
+	        while (amount < prices[song - 1]) {
+	            float additionalAmount;
+	            printf("ENTER YOUR ADDITIONAL MONEY: $"); 
+	            scanf("%f", &additionalAmount);
+	            amount += additionalAmount; 
+		            if (amount < prices[song - 1]) {
+		                printf("STILL NOT ENOUGH. YOU NEED TO ADD: $%.2f\n", prices[song - 1] - amount);
+		            }
+	        }
+        printf("YOUR CHANGE: $%.2f\n", amount - prices[song - 1]);
+    }
+
+    printf("YOU'VE SUCCESSFULLY RENTED THE SONG. NOW PLAYING!\n\n");
+
+    cupofjoe(song);
+
+    return 0;
+}
